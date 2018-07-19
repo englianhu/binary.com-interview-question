522a4369519f2a742b9d5a0c9fb3aa8a5b5abc82
Ryo Eng Lian Hu <englianhu@gmail.com>
2018-07-19 14:31:09 +0000

    updated

--- function/read_HL_tick_data.R
+++ function/read_HL_tick_data.R
@@ -9,15 +9,13 @@ read_HL_tick_data <- function(dr = 'data/USDJPY/', df.type = 'data.table') {
   rm(pkgs)
   
   #'@ dr <- 'data/USDJPY/'
-<<<<<<< HEAD
-  
+
   ## unzip dataset.
   if(file.exists(paste0(dr, 'USDJPY.zip'))) {
     unzip(paste0(dr, 'USDJPY.zip'), exdir = dr)
   }
   
   res <- ldply(paste0(dr, dir(dr, pattern = '_HL.rds')), readRDS)
-=======
   dr1 <- str_replace(dr, '/$', '')
   
   ## unzip dataset.
@@ -27,7 +25,6 @@ read_HL_tick_data <- function(dr = 'data/USDJPY/', df.type = 'data.table') {
   }
   
   res <- ldply(paste0(dr, dir(dr1, pattern = '_HL.rds')), readRDS)
->>>>>>> 625a9d893fbce4db5d2e5c9a40098a32d3380b07
   
   if(df.type == 'data.table') {
     res %<>% data.table
@@ -36,14 +33,9 @@ read_HL_tick_data <- function(dr = 'data/USDJPY/', df.type = 'data.table') {
   } else {
     stop("Kindly choose df.type = 'data.table' or df.type = 'tbl_df'.")
   }
-  
-<<<<<<< HEAD
-  file.remove(paste0(dr, dir(dr, pattern = '_HL.rds')))
-=======
+
   file.remove(paste0(dr, dir(dr1, pattern = '_HL.rds')))
-  
   res %<>% select(Date, DateTime, Bid, Ask)
->>>>>>> 625a9d893fbce4db5d2e5c9a40098a32d3380b07
-  
+
   return(unique(res))
 }
