60c6695d2c29f2cf6a1f5dc1693ff3cae79aee1a
®γσ, Eng Lian Hu <englianhu@gmail.com>
2018-10-07 22:44:57 +0900

    a logo

--- function/convertOHLC.R
+++ function/convertOHLC.R
@@ -1,9 +1,12 @@
-convertOHLC <- function(mbase, .unit = 'minute') {
+convertOHLC <- function(mbase, combine = FALSE, mean = FALSE, 
+                        tz = 'Europe/Athens', .unit = 'minute') {
   
   require('BBmisc')
   pkgs <- c('magrittr', 'lubridate', 'tidyverse', 'tidyquant')
   lib(pkgs)
   rm(pkgs)
+  
+  .unit2 <- .unit %>% str_replace_all('s$', '')
   
   # to.period()
   # Valid period character strings include: "seconds", "minutes", 
@@ -15,19 +18,49 @@ convertOHLC <- function(mbase, .unit = 'minute') {
   mbaseA <- suppressAll(
     mbase %>% 
       dplyr::select(DateTime, Ask) %>% 
-      mutate(DateTime = as.POSIXct(mdy_hms(DateTime))) %>% 
-      tk_xts(.) %>% to.period(period = .unit) %>% 
-      tk_tbl() %>% dplyr::rename_all(str_replace_all, '\\.', '') %>% 
+      mutate(DateTime = .POSIXct(mdy_hms(DateTime), tz = tz)) %>% 
+      tk_xts(.) %>% to.period(period = .unit) %>% tk_tbl() %>% 
+      mutate(index = round_date(index, .unit2)) %>% 
+      dplyr::rename_all(str_replace_all, '\\.', '') %>% 
       dplyr::rename_all(tolower))
+  
+  nch <- mbaseA$index[1] %>% substr(nchar(.)+2, nchar(.)+3)
+  mbaseA %<>% ## https://stackoverflow.com/questions/6668963/how-to-prevent-ifelse-from-turning-date-objects-into-numeric-objects
+      mutate(nch = nch, index = if_else(nch == '23', index + hours(1), index)) %>% 
+      dplyr::select(-nch)
   
   mbaseB <- suppressAll(
     mbase %>% 
       dplyr::select(DateTime, Bid) %>% 
-      mutate(DateTime = as.POSIXct(mdy_hms(DateTime))) %>% 
-      tk_xts(.) %>% to.period(period = .unit) %>% 
-      tk_tbl() %>% dplyr::rename_all(str_replace_all, '\\.', '') %>% 
+      mutate(DateTime = .POSIXct(mdy_hms(DateTime), tz = tz)) %>% 
+      tk_xts(.) %>% to.period(period = .unit) %>% tk_tbl() %>% 
+      mutate(index = round_date(index, .unit2)) %>% 
+      dplyr::rename_all(str_replace_all, '\\.', '') %>% 
       dplyr::rename_all(tolower))
   
-  tmp <- list(Ask = mbaseA, Bid = mbaseB)
+  nch <- mbaseB$index[1] %>% substr(nchar(.)+2, nchar(.)+3)
+  mbaseB %<>% ## https://stackoverflow.com/questions/6668963/how-to-prevent-ifelse-from-turning-date-objects-into-numeric-objects
+    mutate(nch = nch, index = if_else(nch == '23', index + hours(1), index)) %>% 
+    dplyr::select(-nch)
+  
+  if (combine == TRUE) {
+    mbaseA %<>% cbind(Type = 'Ask', .)
+    mbaseB %<>% cbind(Type = 'Bid', .)
+    tmp <- rbind(mbaseA, mbaseB) %>% tbl_df %>% 
+      dplyr::select(index, Type, open, high, low, close) %>% 
+      arrange(index)
+    
+    if (mean == TRUE) {
+      tmp %<>% ddply(.(index), summarise, 
+                     open = mean(open, na.rm=TRUE), 
+                     high = mean(high, na.rm=TRUE), 
+                     low = mean(low, na.rm=TRUE), 
+                     close = mean(close, na.rm=TRUE)) %>% tbl_df
+    }
+    
+  } else {
+    tmp <- list(Ask = mbaseA, Bid = mbaseB)
+  }
+  
   return(tmp)
   }
