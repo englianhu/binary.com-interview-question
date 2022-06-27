8f2cc1a5336aa2ccc39cd8c2388ef952ecd33f48
®γσ, ξηg Lιαη Ημ <englianhu@gmail.com>
2022-06-28 02:12:51 +0800

    更新

--- 函数/季节性自回归.R
+++ 函数/季节性自回归.R
@@ -50,16 +50,17 @@
       if(.模型选项 == '自动化') {
         
         季回归 <- tryCatch({
-          auto.arima(季回归, d = .差分阶数, D = .季节性差分阶数, seasonal = 季节性与否, 
-                     stationary = 静态与否, trace = 记载自回归与否, 
-                     ic = 信息量准则, stepwise = 逐步精化与否, nmodels = 逐步精化量, 
-                     #approximation = 近似值与否, 
-                     method = 计策谋略, truncate = 缩写, #x = y, 
-                     xreg = 趋势, test = 测试, lambda = 博克斯考克斯变换, 
-                     test.args = 测试参数, allowdrift = 允许截距与否, 
-                     seasonal.test = 季节性测试, seasonal.test.args = 季节性测试参数, 
-                     allowmean = 允许包含均值与否, biasadj = 偏差调整与否, 
-                     parallel = 多管齐下与否, num.cores = 核心量)
+          auto.arima(
+            季回归, d = .差分阶数, D = .季节性差分阶数, seasonal = 季节性与否, 
+            stationary = 静态与否, trace = 记载自回归与否, 
+            ic = 信息量准则, stepwise = 逐步精化与否, nmodels = 逐步精化量, 
+            #approximation = 近似值与否, 
+            method = 计策谋略, truncate = 缩写, #x = y, 
+            xreg = 趋势, test = 测试, lambda = 博克斯考克斯变换, 
+            test.args = 测试参数, allowdrift = 允许截距与否, 
+            seasonal.test = 季节性测试, seasonal.test.args = 季节性测试参数, 
+            allowmean = 允许包含均值与否, biasadj = 偏差调整与否, 
+            parallel = 多管齐下与否, num.cores = 核心量)
         }, 错误信息 = function(错误信息参数) NULL)
         
       } else if (.模型选项 == '自回归滑均') {
@@ -68,11 +69,12 @@
         #   as.xts
         
         季回归 <- tryCatch({
-          Arima(季回归, order = .时序规律, seasonal = .季节性规律参数, 
-                xreg = 趋势, include.mean = 包含均值与否, 
-                include.drift = 包含截距, #include.constant = 包含常数, 
-                #model = 统计模型, lambda = 博克斯考克斯变换, x = y, 
-                biasadj = 偏差调整与否, method = 计策谋略)
+          Arima(
+            季回归, order = .时序规律, seasonal = .季节性规律参数, 
+            xreg = 趋势, include.mean = 包含均值与否, 
+            include.drift = 包含截距, #include.constant = 包含常数, 
+            #model = 统计模型, lambda = 博克斯考克斯变换, x = y, 
+            biasadj = 偏差调整与否, method = 计策谋略)
           }, 错误信息 = function(错误信息参数) NULL)
         
       } else {
@@ -101,7 +103,7 @@
           .模型选项, '_差分阶数', .差分阶数, '_季节性差分阶数', 
           .季节性差分阶数, '_季节性与否', 季节性与否, '_数据量', 数据量, 
           '_循环周期', 循环周期, '_预测时间单位', 预测时间单位, 
-          '_', as_date(季回归$年月日时分[1]), '.rds')
+          '_', 培训数据$年月日时分[迭数1], 'CST.rds')
         
       } else if (.模型选项 == '自回归滑均') {
         
@@ -109,7 +111,7 @@
           .模型选项, '_自回归滑动平均', 
           paste(.时序规律, collapse = ''), '_季节性', 
           paste(.季节性规律参数, collapse = ''), '_', 数据量, '_', 
-          预测时间单位, '_', as_date(季回归$年月日时分[1]), '.rds')
+          预测时间单位, '_', 培训数据$年月日时分[迭数1], 'CST.rds')
         
       } else {
         
@@ -149,17 +151,18 @@
       if(.模型选项 == '自动化') {
         
         季回归 <- tryCatch({
-          auto.arima(季回归, d = .差分阶数, D = .季节性差分阶数, seasonal = 季节性与否, 
-                     stationary = 静态与否, trace = 记载自回归与否, 
-                     ic = 信息量准则, stepwise = 逐步精化与否, nmodels = 逐步精化量, 
-                     #approximation = 近似值与否, 
-                     method = 计策谋略, truncate = 缩写, #x = y, 
-                     xreg = 趋势, test = 测试, lambda = 博克斯考克斯变换, 
-                     test.args = 测试参数, allowdrift = 允许截距与否, 
-                     seasonal.test = 季节性测试, 
-                     seasonal.test.args = 季节性测试参数, 
-                     allowmean = 允许包含均值与否, biasadj = 偏差调整与否, 
-                     parallel = 多管齐下与否, num.cores = 核心量)
+          auto.arima(
+            季回归, d = .差分阶数, D = .季节性差分阶数, seasonal = 季节性与否, 
+            stationary = 静态与否, trace = 记载自回归与否, 
+            ic = 信息量准则, stepwise = 逐步精化与否, nmodels = 逐步精化量, 
+            #approximation = 近似值与否, 
+            method = 计策谋略, truncate = 缩写, #x = y, 
+            xreg = 趋势, test = 测试, lambda = 博克斯考克斯变换, 
+            test.args = 测试参数, allowdrift = 允许截距与否, 
+            seasonal.test = 季节性测试, 
+            seasonal.test.args = 季节性测试参数, 
+            allowmean = 允许包含均值与否, biasadj = 偏差调整与否, 
+            parallel = 多管齐下与否, num.cores = 核心量)
         }, 错误信息 = function(错误信息参数) NULL)
         
       } else if (.模型选项 == '自回归滑均') {
@@ -168,12 +171,13 @@
         #   as.xts
         
         季回归 <- tryCatch({
-          Arima(季回归, order = .时序规律, seasonal = .季节性规律参数, 
-                xreg = 趋势, include.mean = 包含均值与否, 
-                include.drift = 包含截距, 
-                #include.constant = 包含常数, 
-                #model = 统计模型, lambda = 博克斯考克斯变换, x = y, 
-                biasadj = 偏差调整与否, method = 计策谋略)
+          Arima(
+            季回归, order = .时序规律, seasonal = .季节性规律参数, 
+            xreg = 趋势, include.mean = 包含均值与否, 
+            include.drift = 包含截距, 
+            #include.constant = 包含常数, 
+            #model = 统计模型, lambda = 博克斯考克斯变换, x = y, 
+            biasadj = 偏差调整与否, method = 计策谋略)
         }, 错误信息 = function(错误信息参数) NULL)
         
       } else {
@@ -185,34 +189,36 @@
         季回归 %<>% 
           forecast::forecast(h = 预测时间单位) %>% 
           tk_tbl %>% 
-          mutate(年月日时分 = 培训数据_测试数据[(.N - 预测时间单位 + 1):.N,]$年月日时分, 
-                      市场价 = 培训数据_测试数据[(.N - 预测时间单位 + 1):.N,]$闭市价) %>% 
+          mutate(
+            年月日时分 = 培训数据_测试数据[(.N - 预测时间单位 + 1):.N,]$年月日时分, 
+            市场价 = 培训数据_测试数据[(.N - 预测时间单位 + 1):.N,]$闭市价) %>% 
           rename(预测价 = `Point Forecast`) %>% 
           select(年月日时分, 市场价, 预测价)
         
         cat('\n-------------------------------------------\n')
-        cat('预测序列[', i, ']\n')
+        cat('预测序列[', 迭数1, ']\n')
         print(季回归 %>% as.data.table)
       }
       
       if(.模型选项 == '自动化') {
         
-        模型名称 <- paste0(.模型选项, '_差分阶数', .差分阶数, '季节性差分阶数', 
-                       .季节性差分阶数, '_季节性与否', 季节性与否, '_', 数据量, '_', 
-                       预测时间单位, '.', as_date(季回归$年月日时分[1]), '.rds')
+        模型名称 <- paste0(
+          .模型选项, '_差分阶数', .差分阶数, '季节性差分阶数', 
+          .季节性差分阶数, '_季节性与否', 季节性与否, '_', 数据量, '_', 
+          预测时间单位, '_', 培训数据$年月日时分[迭数1], 'CST.rds')
         
       } else if (.模型选项 == '自回归滑均') {
-        模型名称 <- paste0(.模型选项, '_自回归滑动平均', 
-                       paste(.时序规律, collapse = ''), '_季节性', 
-                       paste(.季节性规律参数, collapse = ''), '_', 数据量, '_', 
-                       预测时间单位, '.', as_date(季回归$年月日时分[1]), '.rds')
+        模型名称 <- paste0(
+          .模型选项, '_自回归滑动平均', paste(.时序规律, collapse = ''), '_季节性', 
+          paste(.季节性规律参数, collapse = ''), '_', 数据量, '_', 
+          预测时间单位, '_', 培训数据$年月日时分[迭数1], 'CST.rds')
         
       } else {
         
       }
       
       saveRDS(季回归, paste0(路径, '文艺数据库/fx/USDJPY/仓库/季节性自回归_', 模型名称))
-      cat('\n', i, '=', 模型名称, '\n\n')
+      cat('\n', 迭数1, '=', 模型名称, '\n\n')
       rm(季回归)
     }
   })
