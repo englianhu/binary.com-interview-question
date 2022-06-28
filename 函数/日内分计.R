a54a8a6a7b0147193a7e649c85c35ac908194c6f
®γσ, ξηg Lιαη Ημ <englianhu@gmail.com>
2022-06-29 04:09:17 +0800

    修改HFT-RV3, 添加 HFT-RV4
    HFT-RV3
    - 季节性平滑模型
    HFT-RV4
    - 比较其它自回归模型

--- 函数/日内分计.R
+++ 函数/日内分计.R
@@ -1,77 +1,92 @@
 日内分计 <- function(时间索引, 样本 = 样本, 数据量, 路径 = NULL, 
-                      hrz1 = 1, 预测时间单位 = 1, .model, vb = TRUE) {
-  ## 数据量 240, 360, 480; hrz1 = 1
-  intr <- 数据量/hrz1
-  dy <- 1440
+                      循环周期 = 1, 预测时间单位 = 1, .模型选项, 列印 = TRUE) {
   
-  tmp <- llply(1:length(时间索引), function(i) {
+  options(digits = 16)
+  require('dplyr', quietly = TRUE)
+  require('forecast', quietly = TRUE)
+  require('data.table', quietly = TRUE)
+  conflict_prefer('mutate', 'dplyr', quiet = TRUE)
+  conflict_prefer('rename', 'dplyr', quiet = TRUE)
+  conflict_prefer('select', 'dplyr', quiet = TRUE)
+  conflict_prefer('forecast', 'forecast', quiet = TRUE)
+  
+  ## 假设数据量 = 1200分钟
+  ## 循环周期 = 1
+  ## 20小时一周期（1200分钟循环1次）
+  ## 10小时一周期（600分钟循环2次）
+  日分计 <- 数据量/循环周期
+  if(!'data.table' %in% class(样本)) 样本 %<>% as.data.table
+  
+  成品 <- llply(1:length(时间索引), function(迭数1) {
     
-    tmp2 <- llply(1:1440, function(j) {
+    ## 一日 = 1440分钟
+    半成品 <- llply(1:1440, function(迭数2) {
       
-      if(j == 1) {
-        lst_sq <- 样本[date < 时间索引[i],][.N]$sq + 1
+      if(迭数2 == 1) {
+        最终序列 <- 样本[日期 < 时间索引[迭数1],][.N]$序列
         
       } else {
-        lst_sq <- 样本[date < 时间索引[i],][.N]$sq + 1 + hrz1 * (j - 1)
+        最终序列 <- 样本[日期 < 时间索引[迭数1],][.N]$序列 + 循环周期 * (迭数2 - 1)
       }
       
-      train <- 样本[(lst_sq - 数据量 + 1):lst_sq]
-      ctr <- train$sq[1]:(range(train$sq)[2] + hrz1)
+      培训数据 <- 样本[(最终序列 - 数据量 + 1):最终序列]
+      序列号 <- 培训数据$序列[1]:(range(培训数据$序列)[2] + 预测时间单位)
       
-      if(vb == TRUE) {
+      if(列印 == TRUE) {
         cat('\n===========================================\n')
-        cat('train[', i, '-', j, ']\n')
-        print(train)
+        cat('培训数据[', 迭数1, '-', 迭数2, ']\n')
+        print(培训数据)
       }
       
-      train_test <- 样本[sq %in% ctr]
-      if(vb == TRUE) {
+      培训数据_测试数据 <- 样本[序列 %in% 序列号]
+      if(列印 == TRUE) {
         cat('\n-------------------------------------------\n')
-        cat('train_test[', i, '-', j, ']\n')
-        print(train_test)
+        cat('培训数据_测试数据[', 迭数1, '-', 迭数2, ']\n')
+        print(培训数据_测试数据)
       }
       
-      sets <- train[, .(index, close)] %>% 
+      季回归 <- 培训数据[, .(年月日时分, 闭市价)] %>% 
         as_tibble %>% 
-        tk_ts(frequency = hrz1) %>% 
-        ets(model = .model) %>% 
+        tk_ts(frequency = 循环周期) %>% 
+        ets(model = .模型选项) %>% 
         forecast(h = 预测时间单位) %>% 
         tk_tbl %>% 
-        dplyr::mutate(index = train_test[(.N - hrz1 + 1):.N,]$index, 
-                      mk.price = train_test[(.N - hrz1 + 1):.N,]$close) %>% 
-        dplyr::rename(fc.price = `Point Forecast`) %>% 
-        dplyr::select(index, mk.price, fc.price)
+        mutate(年月日时分 = 培训数据_测试数据[.N]$年月日时分, 
+                      市场价 = 培训数据_测试数据[.N]$闭市价) %>% 
+        rename(预测价 = `Point Forecast`) %>% 
+        select(年月日时分, 市场价, 预测价)
       
-      if(vb == TRUE) {
+      if(列印 == TRUE) {
         cat('\n-------------------------------------------\n')
-        cat('forecast[', i, '-', j, ']\n')
-        print(sets %>% as.data.table)
+        cat('forecast[', 迭数1, '-', 迭数2, ']\n')
+        print(季回归 %>% as.data.table)
       }
       
       if(is.null(路径)) {
-        路径 <- paste0(.dtr, '文艺数据库/fx/USDJPY/仓库/ts_ets_', .model, '_', 
-                         数据量, '_', hrz1, '.p_', j, '.', 
-                         as_date(sets$index[1]), '.rds')
-      } else {
-        路径 <- paste0(路径, '/ts_ets_', .model, '_', 
-                         数据量, '_', hrz1, '.p_', j, '.', 
-                         as_date(sets$index[1]), '.rds')
-        saveRDS(sets, 路径)
+        if(!exists('.路径')) {
+          .路径 <- '/home/englianhu/Documents/GitHub/binary.com-interview-question-data/'}
         
-        cat('\n', i, '-', j, '=', 
-            paste0('~/文艺数据库/fx/USDJPY/仓库/ts_ets_', .model, '_', 
-                   数据量, '_', hrz1, '.p_', j, '.', 
-                   as_date(sets$index[1]), '.rds saved!'))
+        路径 <- paste0(
+          .路径, '文艺数据库/fx/USDJPY/仓库/季平滑_', .模型选项, '_', 数据量, '_', 循环周期, '_', 
+          as_date(季回归$年月日时分[1]), 'CST_第', 迭数2, '部_.rds')
+        
+      } else {
+        路径 <- paste0(
+          路径, '/季平滑_', .模型选项, '_', 数据量, '_', 循环周期, '_', 
+          as_date(季回归$年月日时分[1]), 'CST_第', 迭数2, '部_.rds')
       }
-      
+      saveRDS(季回归, 路径)
+      cat('\n', 迭数1, '-', 迭数2, '=', 
+          paste0('~/文艺数据库/fx/USDJPY/仓库/季平滑_', .模型选项, '_', 数据量, '_', 循环周期, '_', 
+                 as_date(季回归$年月日时分[1]), 'CST_第', 迭数2, '部_.rds 已储存!'))
       
       cat('\n\n')
-      rm(sets)
+      rm(季回归)
       gc()
     })
-    return(tmp2)
+    return(半成品)
   })
-  return(tmp)
+  return(成品)
 }
 
 
