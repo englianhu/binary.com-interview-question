6c543f86ca0e26dd45de859ea0dc15835c2ba1dd
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2022-12-11 19:36:48 +0800

    欲导出Gitee，但Gitee容量有限

--- 函数/last.R
+++
@@ -1,7 +0,0 @@
-last <- function (x, length.out = 1, na.rm = FALSE) 
-{
-  if (na.rm) 
-    x <- x[!is.na(x)]
-  n <- length(x)
-  x[sign(length.out) * (n - abs(length.out) + 1):n]
-}
--- 实验室/testRealTimeTransc/function/.Mn.R
+++
@@ -1,5 +0,0 @@
-Mn <- function (x) {
-  if (has.Mn(x)) 
-    return(x[, grep('Mean', colnames(x), ignore.case = TRUE)])
-  stop('subscript out of bounds: no column name containing "Mean"')
-}
