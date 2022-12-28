73000e404d05199a718027c83be2d7f2e4606963
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2022-12-28 19:29:57 +0800

    🚩🦔量化对冲，中科红旗；一带一路，泛亚高铁。
    🚩🦔《大秦赋 - 🇬🇧♰👱🏻‍♀️关雎》
    🚩🦔巫师治国，禍殃全球；
    🚩🦔巫裔尽弃，瓦釜雷鸣。
    🚩🦔商鞅变法，铲除巫裔；
    🚩🦔车裂刘瑾，中华执政。
    🚩🦔一带一路，横跨七洲；
    🚩🦔史无前例，一统天下。
    🚩🦔高频量化，对冲基金；
    🚩🦔只争朝夕，不负韶华。
    🚩🦔学海无涯，唯勤是岸；
    🚩🦔不忘初心，方得始终。

--- 函数/总汇结论.R
+++ 函数/总汇结论.R
@@ -1,4 +1,4 @@
-总汇结论 <- function(文件名 = '日内指数平滑数据', 是否储存结论 = '否') {
+总汇结论 <- function(总汇 = NULL, 文件名 = '日内指数平滑数据', 是否储存结论 = '否') {
   ## 
   ############################ 海纳百川，有容乃大； ############################
   ############################ 三军未动，粮草先行。 ############################
@@ -19,7 +19,7 @@
   .蜀道仓库 <- paste0(.蜀道, '文艺数据库/fx/USDJPY/仓库/')
   
   source('函数/汇总上奏.R')
-  if (!exists('总汇')) {
+  if (!exists('总汇') || is.null('总汇')) {
     总汇 <- readRDS(paste0(.蜀道仓库, 文件名, '总汇.rds'))
   }
   
@@ -27,14 +27,16 @@
     市场价 = 市场价
     预测价 = 预测价
     .SD[, .(.N, 
-            mae = MLmetrics::MAE(y_true = 市场价, y_pred = 预测价), 
-            mape = MLmetrics::MAPE(y_true = 市场价, y_pred = 预测价), 
-            rmse = MLmetrics::RMSE(y_true = 市场价, y_pred = 预测价), 
-            smape = Metrics::smape(actual = 市场价, predicted = 预测价), 
-            mse = MLmetrics::MSE(y_true = 市场价, y_pred = 预测价)), 
+            MAE = MLmetrics::MAE(y_true = 市场价, y_pred = 预测价), 
+            MAPE = MLmetrics::MAPE(y_true = 市场价, y_pred = 预测价), 
+            RMSE = MLmetrics::RMSE(y_true = 市场价, y_pred = 预测价), 
+            SMAPE = Metrics::SMAPE(actual = 市场价, predicted = 预测价), 
+            MSE = MLmetrics::MSE(y_true = 市场价, y_pred = 预测价)), 
         by = .(频率)]}][order(频率), ]
   setorder(结论, 频率)
-  setnames(结论, old = 'N', new = '频率（分计）')
+  setnames(结论, old = c('N', 'MAE', 'MAPE', 'RMSE', 'SMAPE', 'MSE'), 
+           new = c('频率（分计）', '均对误差（MAE）', '均对百分比误差（MAPE）', '均方根误差（RMSE）', '对称均对百分比误差（SMAPE）', '均方误差（MSE）'))
+  结论[, '平均绝对比例误差（MASE）' := NA]
   
   是否储存结论 <- 是否储存结论
   if (!是否储存结论 %in% c('是', '否')) {
