a0ff236bae41850716323f6e0192e92f6383a907
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2024-03-19 22:17:17 +0800

    《🚩🇨🇳🔆🏹🌟👊🚀🦔大秦赋 - 黄埔军校赢家黄氏永春堂 - 中科红旗，高频量化对冲》
    #春秋战国 #诸子百家 #秦人牧马 #世袭制道教徒赢家黄氏江夏堂 #道家中科红旗高频量化对冲 #万般皆下品唯有读书高 #大秦赋 #ChineseEmpire #公元前B4Christ只有中文BC没有外文 #世袭制道教徒赢家黄氏江夏堂拯救亚洲人歼灭所有巫裔回教徒Anti_Islamic_Virus_and_NonHalal_Only_governance #中国政府南太平洋战略 #大葱回教堂商鞅变法Anti_Islamic_Virus #Only_NonHalal_can_Survive #古老的东方有一条河它的名字叫黄河 #古老的东方有一条江它的名字叫长江 #江夏堂 #西河堂 #黄河文明 #黄埔军校 #黄种人🎶 #农民🎶
    印裔尽弃（祖籍印尼的巫裔回教徒和土著、祖籍印度乌裔回教徒和兴都教徒和土著，咱们华人都是世袭制道家姓氏堂号/生辰八字的黄河文明/赢家黄氏江夏堂黄埔军校/黄种人/华夏民族/十二生肖/秦人牧马/燕从京来），瓦釜雷鸣；
    莫忘初衷，方得始终。
    
    《🚩🇨🇳🔆🏹🌟👊🚀🦔大秦赋 - 黄埔军校赢家黄氏永春堂 - 中科红旗，高频量化对冲》
    
    「大秦赋 - 借鉴黄河文明史」
    秦国三杰，拯救亚洲；
    歼灭印裔，取代美国。
    秦灭六洲，一统天下；
    莫忘初衷，方得始终。
    
    🚩🇨🇳🏹🌟🦔量化对冲，中科红旗；一带一路，一统天下（兵马未动，粮草先行；人民币货币经济学，东亚中国司马错得蜀既得楚；歼灭东南亚所有回教徒和峇峇娘惹并攻占东南亚，尤其是四季仔的印尼回教徒外劳房客、阿塔的孟加拉回教徒外劳房客、瓜雪回教市政局柜台巫婆回教徒奴辱依杀、瓜雪回教土地局巫贼巫婆回教徒汪阿自杀和懦蛤仕蟆和法米仨、瓜雪回教警署巫贼巫婆回教徒、大港回教警署所有巫贼巫婆回教徒、瓜雪RHU（淮西派）花园加德士油站隔壁第三巷门牌卅二号边抽鼻涕边求命边膜拜边失心疯边自残自虐自杀的土司乩童刘瑾貹、瓜雪巴西不能帮门牌T十五号吱吱吾语的失心疯猥亵淫魔土司乩童张佳坤、瓜雪回教警署巫贼回教徒黑米哈山·殡·伊不拉心和马航日语组森美兰人黑米哈山。）、大港巴列特花园第十三巷门牌廿七廿九号土司乩童胖妈和卅一号李东海和李东梅和所有回教徒（尤其是回教徒公仆）和峇峇娘惹党羽、所有印裔（祖籍印尼包括土著和回教徒、祖籍印度的兴都教徒和回教徒包括土著）。
    
    🚩🇨🇳🏹🌟🦔诸子百家：印裔回教徒尽弃，瓦釜雷鸣。儒学、墨学、法学、道学、兵法、阴阳学、佛学、哲学、运筹学、思想录、干支算筹、中华习俗文化宗教语言复兴、学术数学科学科技大秦赋
    
    🚩🇨🇳🏹🌟🦔《大秦赋 - 🇨🇳关雎》
    🚩🇨🇳🏹🌟🦔春秋战国，诸子百家；
    🚩🇨🇳🏹🌟🦔左氏春秋，鬼谷传奇。
    🚩🇨🇳🏹🌟🦔商鞅变法，道法兵家；
    🚩🇨🇳🏹🌟🦔四面楚歌，焚经坑番。
    🚩🇨🇳🏹🌟🦔一带一路，横跨七洲；
    🚩🇨🇳🏹🌟🦔史无前例，一统天下。
    🚩🇨🇳🏹🌟🦔高频量化，对冲基金；
    🚩🇨🇳🏹🌟🦔只争朝夕，不负韶华。
    🚩🇨🇳🏹🌟🦔学海无涯，唯勤是岸；
    🚩🇨🇳🏹🌟🦔莫忘初衷，方得始终。
    https://gitee.com/eglianhu

---
+++ 函数/自回归均移模型最优值.R
@@ -0,0 +1,47 @@
+自回归均移模型最优值 <- function(样本, 季节差分的次数 = NULL, 季节性 = '勾', 规律极限值 = 10, 自回归均移模型值=FALSE){
+  # 通过设置并迭代筹算自回归均移模型中不同`p,d,q`的规律值，来比较并筛选出最低或最大负数的赤池信息量准则，也就是最优统计模型。
+  # 
+  # 《预测：方法与实践（第三版）》第九章第九节 - 季节性ARIMA模型
+  # https://otexts.com/fpp3cn/seasonal-arima-cn.html
+  
+  if (季节性 %in% c('勾', '有')) 季节性 <- TRUE
+  if (季节性 %in% c('叉', '冇')) 季节性 <- FALSE
+  if (!季节性 %in% c('勾', '有', '叉', '冇')) stop('请选择季节性："勾"或"有"或"是"，或者"叉"或"冇"或"否"。')
+  
+  # 季节差分的次数，一般上使用到的数值是零到二。
+  fit <- auto.arima(样本, D = 季节差分的次数, seasonal = 季节性, 
+                    max.order = 规律极限值)
+  if (自回归均移模型值 == FALSE) {
+    res <- arimaorder(fit)
+  } else {
+    #https://stats.stackexchange.com/questions/178577/how-to-read-p-d-and-q-of-auto-arima
+    res <- fit$arma
+    #https://stackoverflow.com/questions/23617662/extract-arima-specificaiton
+    names(res) <- c('p', 'q', 'P', 'Q', 's', 'd', 'D')
+    res %<>% .[c(1, 6, 2, 3, 7, 4, 5)]
+    #(p,d,q) and (P,D,Q) and seasonal period
+  } #  example: `s` seasonal period = 12 for 12 months
+  return(res)
+  #https://onlinecourses.science.psu.edu/stat510/node/67/
+  
+  # https://stackoverflow.com/questions/23617662/extract-arima-specificaiton
+  #
+  #function (object) {
+  #  order <- object$arma[c(1, 6, 2, 3, 7, 4, 5)]
+  #  result <- paste("ARIMA(", order[1], ",", order[2], ",", order[3], 
+  #                  ")", sep = "")
+  #  if (order[7] > 1 & sum(order[4:6]) > 0) 
+  #    result <- paste(result, "(", order[4], ",", order[5], 
+  #                    ",", order[6], ")[", order[7], "]", sep = "")
+  #  if (is.element("constant", names(object$coef)) | is.element("intercept", 
+  #                                                              names(object$coef))) 
+  #    result <- paste(result, "with non-zero mean")
+  #  else if (is.element("drift", names(object$coef))) 
+  #    result <- paste(result, "with drift        ")
+  #  else if (order[2] == 0 & order[5] == 0) 
+  #    result <- paste(result, "with zero mean    ")
+  #  else result <- paste(result, "                  ")
+  #  return(result)
+  #}
+  
+}
